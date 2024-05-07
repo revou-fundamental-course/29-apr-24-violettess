72b3856fa4639f6a6e177e8dd92c578a620d9886
Anabelle Imandjaja <abyimandjaja@gmail.com>
2024-05-07 21:53:45 +0700

    init commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-//Ini JavaScript
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function validation() {
-    console.log('Good')
+function plusDivs(n) {
+  showSlides(slideIndex += n);
 }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("img-slideshow");
+  var texts = document.getElementsByClassName("text-slideshow");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < texts.length; i++) {
+      texts[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  texts[slideIndex-1].style.display = "block";  
+}
+
+// Automatic slideshow
+var slideInterval = setInterval(function() {
+  plusDivs(1);
+}, 3000); 
