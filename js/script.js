fa3f892ad3b86f5721c7e76ab0e1b688d622d2c9
Anabelle Imandjaja <abyimandjaja@gmail.com>
2024-05-09 21:38:28 +0700

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
-//Form Contact Us implements JavaScript Validation.
+// Form validation
 function validateForm() {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let selection = document.getElementById("selection").value;
+  let isValid = true;
+
+// Reset error messages
+document.getElementById("name-error").innerText = "";
+document.getElementById("email-error").innerText = "";
+document.getElementById("selection-error").innerText = "";
+  
+  if (name.trim() === "") {
+      document.getElementById("name-error").innerText = "*Please fill in your name";
+      isValid = false;
+  }
+  
+  if (email.trim() === "") {
+      document.getElementById("email-error").innerText = "*Please fill in your email";
+      isValid = false;
+  } else {
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+          document.getElementById("email-error").innerText = "*Please enter a valid email address";
+          isValid = false;
+      }
+  }
+  
+  if (selection.trim() === "") {
+      document.getElementById("selection-error").innerText = "*Please select an option";
+      isValid = false;
+  }
+  
+  if (isValid) {
+       document.getElementById("success-message").innerText = "Form is submitted";
+  }
+  
   console.log(name)
-
-  if (name.trim() === "" || email.trim() === "" || selection.trim() === "") {
-      alert("Please fill out all fields.");
-      return false;
-  }
-
-  // Validate email format
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-      alert("Please enter a valid email address.");
-      return false;
-  }
-
-  return true;
+  console.log(email)
+  console.log(selection)
+  return isValid;
 }
+
 document.getElementById("button-send").addEventListener("click", () => validateForm())
 
-// Banner use auto slide using JavaScript
+// Auto-slide banner
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -33,17 +56,17 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("img-slideshow");
   var texts = document.getElementsByClassName("text-slideshow");
-  
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
   for (i = 0; i < texts.length; i++) {
       texts[i].style.display = "none";  
   }
-  
+
   slides[slideIndex-1].style.display = "block";  
   texts[slideIndex-1].style.display = "block";  
 }
@@ -51,4 +74,4 @@ function showSlides(n) {
 // Automatic slideshow
 var slideInterval = setInterval(function() {
   plusDivs(1);
-}, 3000); 
+}, 3000);
